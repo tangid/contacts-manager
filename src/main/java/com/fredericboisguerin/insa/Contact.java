91e91605f5729877559f3f2937e34016ca1bfdf4
M. Delanoe Tangi <delanoe@insa-07573>
2017-11-17 09:12:02 +0100

    et de 4 :D

--- src/main/java/com/fredericboisguerin/insa/Contact.java
+++ src/main/java/com/fredericboisguerin/insa/Contact.java
@@ -1,7 +1,36 @@
 package com.fredericboisguerin.insa;
 
 public class Contact {
-        String name;
-        String email;
-        String phoneNumber;
+        public String name;
+        public String email;
+        public String phoneNumber;
+
+        public Contact(String nom, String mail, String tel)
+        {
+                this.name=nom;
+                this.email=mail;
+                this.phoneNumber=tel;
+        }
+
+        public void printContact()
+        {
+                if ((this.email != null) && (this.phoneNumber != null))
+                        System.out.print(this.name + " " + this.email + " " + this.phoneNumber);
+                else if ((this.email != null) && (this.phoneNumber == null))
+                        System.out.print(this.name + " " + this.email);
+                else if ((this.email == null) && (this.phoneNumber != null))
+                        System.out.print(this.name + " " + this.phoneNumber);
+        }
+
+        public String getEmail() {
+                return email;
+        }
+
+        public String getName() {
+                return name;
+        }
+
+        public String getPhoneNumber() {
+                return phoneNumber;
+        }
 }
--- src/main/java/com/fredericboisguerin/insa/ContactsManager.java
+++ src/main/java/com/fredericboisguerin/insa/ContactsManager.java
@@ -1,13 +1,30 @@
+
 package com.fredericboisguerin.insa;
+import java.util.ArrayList;
+
 
 public class ContactsManager {
+    private int maxContact = 0;
+    private ArrayList<Contact> allContact = new ArrayList<Contact>();
+
 
     public void addContact(String name, String email, String phoneNumber) {
-
+        allContact.add(new Contact(name, email, phoneNumber));
+        maxContact = maxContact + 1;
     }
 
-    public void printAllContacts() {
-        System.out.print("Nicole Ferroni");
+    public void printAllContacts()
+    {
+        for (Contact contact: allContact)
+        {
+            if ((contact.email != null) && (contact.phoneNumber != null))
+                System.out.print(contact.name + ", " + contact.email + ", " + contact.phoneNumber);
+            else if ((contact.email != null) && (contact.phoneNumber == null))
+                System.out.print(contact.name + ", " + contact.email);
+            else if ((contact.email == null) && (contact.phoneNumber != null))
+                System.out.print(contact.name + ", " + contact.phoneNumber);
+        }
+
     }
 
     public void searchContactByName(String name) {
